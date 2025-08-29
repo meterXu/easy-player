93182690c3e12eaee846a24b29f041b62ba0c2bd
isaac <xhgainxq@qq.com>
2025-08-29 23:52:05 +0800

    project init

--- src/packages/easy-player/src/core/easy-player.ts
+++ src/packages/easy-player/src/core/easy-player.ts
@@ -5,19 +5,24 @@ export class easyPlayer {
     private readonly player: any = null;
     private config: EasyPlayerConfig = {};
 
-    constructor(container: HTMLElement, config?: EasyPlayerConfig) {
+    constructor(container: HTMLElement, config: EasyPlayerConfig) {
         this.config = merge({}, defaultConfig, config)
         this.player = new window.EasyPlayerPro(container, config);
     }
 
-    play(url:string):Promise<void>{
+    play(url: string): Promise<void> {
         return new Promise((resolve, reject) => {
-            if(!this.player){
+            if (this.player) {
                 this.player.play(url).then(resolve).catch(reject)
-            }else{
+            } else {
                 reject('player not found!');
             }
         })
     }
 
+    setMute(isMute: boolean) {
+        if (!this.player)
+            return
+        this.player.setMute(isMute)
+    }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref, onMounted} from 'vue'
+import {easyPlayer} from "@/packages/easy-player/src/index.ts";
+
+const playerRef = ref()
+const isMute = ref(true)
+let player = null as any
+onMounted(() => {
+  player = new easyPlayer(playerRef.value, {
+    hasAudio: true,
+    stretch: false,
+  })
+  player.play('https://sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/hls/xgplayer-demo.m3u8')
+})
+
+function toggleMute() {
+  isMute.value = !isMute.value
+  player.setMute(isMute.value)
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main style="width: 1200px;">
+    <button @click="toggleMute">静音切换,{{isMute}}</button>
+    <div class="player_box" ref="playerRef"></div>
   </main>
 </template>
+
+<style>
+.player_box {
+  position: relative;
+  padding-bottom: 56%;
+  background-color: #000;
+  border: 1px #181818 solid;
+}
+</style>
