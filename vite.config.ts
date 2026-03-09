3b5c9b52fd38244fe67c1c08345d9f14a48f5cfd
isaac <xhgainxq@qq.com>
2026-03-09 14:14:48 +0800

    feat: 按环境变量配置 Vite base 并新增生产/开发 env 文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import easyPlayerPro from "./src/packages/vite-plugin-easy-player-pro/src/index"
+import easyPlayerPro from './src/packages/vite-plugin-easy-player-pro/src/index'
+import { loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-      //@ts-ignore
-    easyPlayerPro({
-      easyPlayerBuildRootPath:'src/packages/easy-player-pro/src',
-      easyPlayerBuildDir:'build'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    base: env.VITE_APP_base,
+    plugins: [
+      vue(),
+      vueJsx(),
+      easyPlayerPro({
+        easyPlayerBuildRootPath: 'src/packages/easy-player-pro/src',
+        easyPlayerBuildDir: 'build'
+      }) as any
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: true
-  },
+    server: {
+      host: '0.0.0.0',
+      allowedHosts: true as true
+    }
+  }
 })
